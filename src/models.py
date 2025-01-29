5bac1b17065834f3d188da7cef7339d0c3c1cdd4
Pablo Teran <144842209+Devnaret0399@users.noreply.github.com>
2025-01-29 03:00:00 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -2,18 +2,96 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Person(db.Model):
+    __tablename__ = 'person'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
+    name = db.Column(db.String(80), nullable=False)
+    hair_color = db.Column(db.String(80), nullable=False)
+    eye_color = db.Column(db.String(80), nullable=False)
+    skin_color = db.Column(db.String(80), nullable=False)
+    gender = db.Column(db.String(80), nullable=False)
+    specie = db.Column(db.String(80), nullable=False)
+    home_world = db.Column(db.String(80), nullable=False)
+    height = db.Column(db.Integer, nullable=False)
+    mass = db.Column(db.Integer, nullable=False)
 
     def serialize(self):
         return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            'id': self.id,
+            'name': self.name,
+            'hair_color': self.hair_color,
+            'eye_color': self.eye_color,
+            'skin_color': self.skin_color,
+            'gender': self.gender,
+            'specie': self.specie,
+            'home_world': self.home_world,
+            'height': self.height,
+            'mass': self.mass
+        }
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    diameter = db.Column(db.Integer, nullable=False)
+    rotation_period = db.Column(db.Integer, nullable=False)
+    orbital_period = db.Column(db.Integer, nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+    surface_water = db.Column(db.Integer, nullable=False)
+    gravity = db.Column(db.String(80), nullable=False)
+    climate = db.Column(db.String(80), nullable=False)
+    terrain = db.Column(db.String(80), nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'diameter': self.diameter,
+            'rotation_period': self.rotation_period,
+            'orbital_period': self.orbital_period,
+            'population': self.population,
+            'surface_water': self.surface_water,
+            'gravity': self.gravity,
+            'climate': self.climate,
+            'terrain': self.terrain
+        }
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email
+        }
+
+class FavoritePeople(db.Model):
+    __tablename__ = 'favorite_people'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    person_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'person_id': self.person_id
+        }
+
+class FavoritePlanet(db.Model):
+    __tablename__ = 'favorite_planets'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'planet_id': self.planet_id
         }
